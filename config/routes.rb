884f175042362ec976bf2ee2a4b88a29e9b485a2
Seulkee Kang <seulkeekang@gmail.com>
2022-04-11 09:24:06 +0100

    add dashboard controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   root to: "pages#home"
 
+  get 'dashboard', to: 'pages#dashboard', as: :dashboard
+
   resources :users, only: [:index, :show] do
     resources :donors, only: %i[new create edit update]
     resources :scholars, only: %i[new create edit update]
