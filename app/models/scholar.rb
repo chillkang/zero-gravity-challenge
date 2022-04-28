1b3e8d55064e9ead815874204a5acd1793697c54
Seulkee Kang <seulkeekang@gmail.com>
2022-04-28 09:27:21 +0100

    update styling

--- app/models/scholar.rb
+++ app/models/scholar.rb
@@ -1,7 +1,6 @@
 class Scholar < ApplicationRecord
   belongs_to :user
   has_many :transactions
-  has_many :donors, through: :transactions
-
+  
   validates :institution_name, :bank_name, :bank_account, presence: true
 end
